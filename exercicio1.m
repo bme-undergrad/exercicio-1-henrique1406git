6f3141618e6ba9bd9de671eafdfe49b4bd447927
Henrique Machiaveli Martins <hnriquemartins1406@gmai.com>
2025-08-27 20:50:29 -0300

    Segundo commit

--- exercicio1.m
+++ exercicio1.m
@@ -7,11 +7,12 @@ imax = 20;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-t = zeros(20, 1);
-t(1) = 0.1;
-erro = zeros(length(t), 1);
 
-for ii = 1:length(t)-1 %usar ii e n i pq i eh imaginarium
+t = zeros(20, 1);
+t(1) = x0;
+erro = zeros(imax, 1);
+
+for ii = 1:imax %usar ii e n i pq i eh imaginarium
   if ii ~= 1
     erro(ii) = abs(t(ii) - t(ii-1))/t(ii); %abs é modulo
     if erro(ii) < es
@@ -20,14 +21,13 @@ for ii = 1:length(t)-1 %usar ii e n i pq i eh imaginarium
   endif
   t(ii+1) = t(ii) - func(t(ii))/func_d(t(ii)); %; faz sumir
 endfor
-  last_index = find(t, 1, 'last');
-  resultado = t(last_index)
 
-  erro = erro(ii)
-
-  resultado_interacoes = ii
-t = 1; % alterar
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%
+resultado = t(ii + 1) 
+erro = erro(ii)
+resultado_interacoes = ii
+t = resultado; % alterar
 
 endfunction
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
