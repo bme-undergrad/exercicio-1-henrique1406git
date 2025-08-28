3ed144357da84cb358c2ac3e6f2ca957f254ae6e
Henrique Machiaveli Martins <hnriquemartins1406@gmai.com>
2025-08-27 21:01:44 -0300

    Terceiro commit

--- exercicio1.m
+++ exercicio1.m
@@ -7,27 +7,46 @@ imax = 20;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
-t = zeros(20, 1);
-t(1) = x0;
+% ANOTAÇÃO: O tamanho do vetor 't' deve ser baseado em imax para evitar erros.
+% Usamos imax+1 para ter espaço para o valor inicial (x0) e mais imax iterações.
+t = zeros(imax + 1, 1);
 erro = zeros(imax, 1);
 
-for ii = 1:imax %usar ii e n i pq i eh imaginarium
-  if ii ~= 1
-    erro(ii) = abs(t(ii) - t(ii-1))/t(ii); %abs é modulo
+% ANOTAÇÃO: Erro #2 corrigido.
+% Usar o x0 fornecido como ponto de partida, em vez de um valor fixo.
+t(1) = x0;;
+
+% Inicializa uma variável para o número de iterações.
+num_iteracoes = 0;
+
+% ANOTAÇÃO: Erro #3 corrigido.
+% O loop deve ir de 1 até o número máximo de iterações (imax).
+for ii = 1:imax
+    % Calcula o próximo valor de t usando a fórmula de Newton
+    t(ii+1) = t(ii) - func(t(ii))/func_d(t(ii));
+    num_iteracoes = ii; % Atualiza o contador de iterações
+
+    % Calcula o erro relativo percentual
+    % Evita divisão por zero se o novo valor for 0
+    if t(ii+1) ~= 0
+        erro(ii) = abs((t(ii+1) - t(ii)) / t(ii+1));
+    else
+        erro(ii) = abs(t(ii+1) - t(ii)); % Usa erro absoluto se t(ii+1) for 0
+    end
+
+    % Verifica o critério de parada (erro menor que o especificado)
     if erro(ii) < es
-      break
-    endif
-  endif
-  t(ii+1) = t(ii) - func(t(ii))/func_d(t(ii)); %; faz sumir
+        break; % Se o erro for pequeno o suficiente, para o loop.
+    end
 endfor
 
-resultado = t(ii + 1) 
-erro = erro(ii)
-resultado_interacoes = ii
-t = resultado; % alterar
+
+% ANOTAÇÃO: Erro #1 corrigido.
+% A função deve retornar o resultado calculado, e não um valor fixo.
+t = resultado;
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 endfunction
-
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%
